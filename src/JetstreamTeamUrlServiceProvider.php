d5da942f2db405dfb329e46caf875c6c8fa24229
Daniel Palmieri <dlspalmieri@gmail.com>
2024-05-03 16:01:31 -0300

    jetstream middleware

--- src/JetstreamTeamUrlServiceProvider.php
+++ src/JetstreamTeamUrlServiceProvider.php
@@ -61,7 +61,6 @@ class JetstreamTeamUrlServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('jetstream-team-url')
-            ->hasConfigFile()
-            ->hasViews();
+            ->hasConfigFile();
     }
 }
